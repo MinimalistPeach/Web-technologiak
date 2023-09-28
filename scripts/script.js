69acca9a7991c34ee21f67e0b6c4160a8deb38d9
MinimalistPeach <stremler.lacika77@gmail.com>
2023-09-28 17:40:01 +0200

    finishing the website

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,32 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
+
     const image = document.getElementById("capybara-image");
     const prevButton = document.getElementById("prev-button");
     const nextButton = document.getElementById("next-button");
 
     // Képek források
-    const imageSources = ["images/capybara.jpg", "images/capybara2.jpg"];
+    const imageSources = ['images/capybara.jpg', 'images/capybara2.jpg', 'images/capybara3.jpg', 'images/capybara4.jpg', 'images/capybara5.jpg'];
+
     let currentImageIndex = 0;
 
     // Függvény a kép forrásának beállításához
     function setImageSource(index) {
-        image.src = imageSources[index];
+        image.style.opacity = 0;
+
+        setTimeout(() => {
+            image.src = imageSources[index];
+            image.style.opacity = 1;
+        }, 250); //250 ms időzítés a képek váltása között
     }
 
     // Előző kép megjelenítése
-    prevButton.addEventListener("click", function() {
+    prevButton.addEventListener("click", function () {
         currentImageIndex = (currentImageIndex - 1 + imageSources.length) % imageSources.length;
         setImageSource(currentImageIndex);
     });
 
     // Következő kép megjelenítése
-    nextButton.addEventListener("click", function() {
+    nextButton.addEventListener("click", function () {
         currentImageIndex = (currentImageIndex + 1) % imageSources.length;
         setImageSource(currentImageIndex);
     });
