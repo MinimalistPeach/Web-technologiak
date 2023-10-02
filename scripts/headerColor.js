6a6bebb6d507586878d2bfa6d87899ec33c068ea
MinimalistPeach <stremler.lacika77@gmail.com>
2023-10-02 15:53:56 +0200

    made text change color

--- scripts/headerColor.js
+++ scripts/headerColor.js
@@ -1,19 +1,34 @@
 colorChooser.addEventListener('input', function () {
     const header = document.querySelector("header");
+    const changables = document.getElementsByClassName("changable");
+    const color = document.getElementById("colorChooser").value;
+    const navbar = document.getElementById("navbar");
 
-    header.style.backgroundColor = document.getElementById("colorChooser").value;
+    for (var i = 0; i < changables.length; i++) {
+        changables[i].style.color = color;
+    }
 
-    localStorage.setItem("headerColor", document.getElementById("colorChooser").value);
+    navbar.style.backgroundColor = color;
+
+    header.style.backgroundColor = color;
+
+    localStorage.setItem("pageColor", color);
 });
 
 document.addEventListener("DOMContentLoaded", function () {
     const header = document.querySelector("header");
+    const navbar = document.getElementById("navbar");
+    const changables = document.getElementsByClassName("changable");
 
-    const storedColor = localStorage.getItem("headerColor");
+    const storedColor = localStorage.getItem("pageColor");
 
     // Ha megtalálja a helyi tárban a színt, akkor beállítja a fejlecet
     if (storedColor) {
         header.style.backgroundColor = storedColor;
         document.getElementById("colorChooser").value = storedColor;
+        navbar.style.backgroundColor = storedColor;
+        for (var i = 0; i < changables.length; i++) {
+            changables[i].style.color = storedColor;
+        }
     }
 });
